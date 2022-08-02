154626fc1fbfbcddf6d504e64f0dfd6558f5e2e3
Christian <3003html@gmail.com>
2022-08-02 13:44:20 -0500

    gRPC unary methods implemented in server

---
+++ server-exam/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/ChrisCodeX/gRPC/database"
+	"github.com/ChrisCodeX/gRPC/exampb"
+	"github.com/ChrisCodeX/gRPC/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":5070")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var postgresURL string = "postgres://postgres:postgres@localhost:54321/postgres?sslmode=disable"
+	// Instance of Postgres Repository
+	repo, err := database.NewPostgreRepository(postgresURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Instance of Student Server
+	server := server.NewExamServer(repo)
+
+	/*gRPC*/
+	// gRPC Server
+	s := grpc.NewServer()
+
+	// Register server on the gRPC
+	exampb.RegisterExamServiceServer(s, server)
+
+	// Register server reflection service on the gRPC
+	reflection.Register(s)
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
+}
--- server/server.go
+++ server/server.go
@@ -3,26 +3,28 @@ package server
 import (
 	"context"
 
+	"github.com/ChrisCodeX/gRPC/exampb"
 	"github.com/ChrisCodeX/gRPC/models"
 	"github.com/ChrisCodeX/gRPC/repository"
 	"github.com/ChrisCodeX/gRPC/studentpb"
 )
 
-// Server struct for student service
+// Server struct for services
 type Server struct {
 	repo repository.Repository
 	studentpb.UnimplementedStudentServiceServer
+	exampb.UnimplementedExamServiceServer
 }
 
-// Server Constructor
+/*Student Service*/
+// Assign database to Student Server
 func NewStudentServer(repo repository.Repository) *Server {
 	return &Server{
 		repo: repo,
 	}
 }
 
-// Methods to student service (Defined in protobuf file)
-// Protobuffer
+// Methods to Student Service (Defined in protobuf file)
 // Request to Get a Student
 func (s *Server) GetStudent(ctx context.Context, req *studentpb.GetStudentRequest) (*studentpb.Student, error) {
 	// Get student from database
@@ -39,7 +41,7 @@ func (s *Server) GetStudent(ctx context.Context, req *studentpb.GetStudentReques
 	}, nil
 }
 
-//
+// Request to Set a Student
 func (s *Server) SetStudent(ctx context.Context, req *studentpb.Student) (*studentpb.SetStudentResponse, error) {
 	// Map student protobuf into student struct
 	student := &models.Student{
@@ -57,5 +59,49 @@ func (s *Server) SetStudent(ctx context.Context, req *studentpb.Student) (*stude
 	// Return SetStudentResponse protobuf
 	return &studentpb.SetStudentResponse{
 		Id: student.Id,
+	}, nil
+}
+
+/*Exam Service*/
+// Assign database to Exam Server
+func NewExamServer(repo repository.Repository) *Server {
+	return &Server{
+		repo: repo,
+	}
+}
+
+//Exams Service Methods
+// Request to Get a Exam
+func (s *Server) GetExam(ctx context.Context, req *exampb.GetExamRequest) (*exampb.Exam, error) {
+	// Get exam from database
+	exam, err := s.repo.GetExam(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	// Map student struct into student protobuf
+	return &exampb.Exam{
+		Id:   exam.Id,
+		Name: exam.Name,
+	}, nil
+}
+
+// Request to Set a Exam
+func (s *Server) SetExam(ctx context.Context, req *exampb.Exam) (*exampb.SetExamResponse, error) {
+	// Map student protobuf into student struct
+	exam := &models.Exam{
+		Id:   req.GetId(),
+		Name: req.GetName(),
+	}
+
+	// Send student to database
+	err := s.repo.SetExam(ctx, exam)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return SetStudentResponse protobuf
+	return &exampb.SetExamResponse{
+		Id: exam.Id,
 	}, nil
 }
