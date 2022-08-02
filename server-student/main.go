9ad6d445fc405e8558749a4962bc78eea72c44a3
Christian <3003html@gmail.com>
2022-08-02 10:11:59 -0500

    server for student service created

---
+++ server-student/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/ChrisCodeX/gRPC/database"
+	"github.com/ChrisCodeX/gRPC/server"
+	"github.com/ChrisCodeX/gRPC/studentpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var postgresURL string = "postgres://postgres:postgres@localhost:54321/postgres?sslmode=disable"
+
+func main() {
+	// Listen announces
+	listener, err := net.Listen("tcp", ":5060")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Instance of Postgres Repository
+	repo, err := database.NewPostgreRepository(postgresURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Instance of Student Server
+	server := server.NewStudentServer(repo)
+
+	/*gRPC*/
+	// gRPC Server
+	s := grpc.NewServer()
+
+	// Register server on the gRPC
+	studentpb.RegisterStudentServiceServer(s, server)
+
+	// Register server reflection service on the gRPC
+	reflection.Register(s)
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
+}
