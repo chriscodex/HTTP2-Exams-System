28ac5091eb0883c72329e8de6a8f333c75a136cc
Christian <3003html@gmail.com>
2022-08-03 11:39:09 -0500

    protobuf package and grpc generated for exam.proto

--- exampb/exam_grpc.pb.go
+++ exampb/exam_grpc.pb.go
@@ -33,6 +33,8 @@ type ExamServiceClient interface {
 	EnrollStudents(ctx context.Context, opts ...grpc.CallOption) (ExamService_EnrollStudentsClient, error)
 	// Stream for server - 1 response from client
 	GetStudentsPerExam(ctx context.Context, in *GetStudentsPerExamRequest, opts ...grpc.CallOption) (ExamService_GetStudentsPerExamClient, error)
+	// Stream Bidirectional
+	TakeExam(ctx context.Context, opts ...grpc.CallOption) (ExamService_TakeExamClient, error)
 }
 
 type examServiceClient struct {
@@ -161,6 +163,37 @@ func (x *examServiceGetStudentsPerExamClient) Recv() (*studentpb.Student, error)
 	return m, nil
 }
 
+func (c *examServiceClient) TakeExam(ctx context.Context, opts ...grpc.CallOption) (ExamService_TakeExamClient, error) {
+	stream, err := c.cc.NewStream(ctx, &ExamService_ServiceDesc.Streams[3], "/exam.ExamService/TakeExam", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &examServiceTakeExamClient{stream}
+	return x, nil
+}
+
+type ExamService_TakeExamClient interface {
+	Send(*TakeExamRequest) error
+	Recv() (*Question, error)
+	grpc.ClientStream
+}
+
+type examServiceTakeExamClient struct {
+	grpc.ClientStream
+}
+
+func (x *examServiceTakeExamClient) Send(m *TakeExamRequest) error {
+	return x.ClientStream.SendMsg(m)
+}
+
+func (x *examServiceTakeExamClient) Recv() (*Question, error) {
+	m := new(Question)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // ExamServiceServer is the server API for ExamService service.
 // All implementations must embed UnimplementedExamServiceServer
 // for forward compatibility
@@ -175,6 +208,8 @@ type ExamServiceServer interface {
 	EnrollStudents(ExamService_EnrollStudentsServer) error
 	// Stream for server - 1 response from client
 	GetStudentsPerExam(*GetStudentsPerExamRequest, ExamService_GetStudentsPerExamServer) error
+	// Stream Bidirectional
+	TakeExam(ExamService_TakeExamServer) error
 	mustEmbedUnimplementedExamServiceServer()
 }
 
@@ -196,6 +231,9 @@ func (UnimplementedExamServiceServer) EnrollStudents(ExamService_EnrollStudentsS
 }
 func (UnimplementedExamServiceServer) GetStudentsPerExam(*GetStudentsPerExamRequest, ExamService_GetStudentsPerExamServer) error {
 	return status.Errorf(codes.Unimplemented, "method GetStudentsPerExam not implemented")
+}
+func (UnimplementedExamServiceServer) TakeExam(ExamService_TakeExamServer) error {
+	return status.Errorf(codes.Unimplemented, "method TakeExam not implemented")
 }
 func (UnimplementedExamServiceServer) mustEmbedUnimplementedExamServiceServer() {}
 
@@ -319,6 +357,32 @@ func (x *examServiceGetStudentsPerExamServer) Send(m *studentpb.Student) error {
 	return x.ServerStream.SendMsg(m)
 }
 
+func _ExamService_TakeExam_Handler(srv interface{}, stream grpc.ServerStream) error {
+	return srv.(ExamServiceServer).TakeExam(&examServiceTakeExamServer{stream})
+}
+
+type ExamService_TakeExamServer interface {
+	Send(*Question) error
+	Recv() (*TakeExamRequest, error)
+	grpc.ServerStream
+}
+
+type examServiceTakeExamServer struct {
+	grpc.ServerStream
+}
+
+func (x *examServiceTakeExamServer) Send(m *Question) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+func (x *examServiceTakeExamServer) Recv() (*TakeExamRequest, error) {
+	m := new(TakeExamRequest)
+	if err := x.ServerStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // ExamService_ServiceDesc is the grpc.ServiceDesc for ExamService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -350,6 +414,12 @@ var ExamService_ServiceDesc = grpc.ServiceDesc{
 			StreamName:    "GetStudentsPerExam",
 			Handler:       _ExamService_GetStudentsPerExam_Handler,
 			ServerStreams: true,
+		},
+		{
+			StreamName:    "TakeExam",
+			Handler:       _ExamService_TakeExam_Handler,
+			ServerStreams: true,
+			ClientStreams: true,
 		},
 	},
 	Metadata: "exampb/exam.proto",
