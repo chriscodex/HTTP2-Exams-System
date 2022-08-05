addcda95924acc813312704fdb9b4f3078acc651
Christian <3003html@gmail.com>
2022-08-05 18:20:13 -0500

    table answers deleted

--- database/up.sql
+++ database/up.sql
@@ -16,6 +16,8 @@ CREATE TABLE exams (
 DROP TABLE IF EXISTS enrollments;
 
 CREATE TABLE enrollments (
+    id VARCHAR(32) PRIMARY KEY,
+    score VARCHAR(32),
     fk_student_id VARCHAR(32) NOT NULL,
     fk_exam_id VARCHAR(32) NOT NULL,
     FOREIGN KEY (fk_student_id) REFERENCES students(id),
@@ -28,15 +30,6 @@ CREATE TABLE questions (
     id VARCHAR(32) PRIMARY KEY,
     fk_exam_id VARCHAR(32) NOT NULL,
     question VARCHAR(255) NOT NULL,
+    answer VARCHAR(255) NOT NULL,
     FOREIGN KEY (fk_exam_id) REFERENCES exams(id)
 );
-
-DROP TABLE IF EXISTS answers;
-
-CREATE TABLE answers (
-    id VARCHAR(32) PRIMARY KEY,
-    fk_question_id VARCHAR(32) NOT NULL,
-    answer VARCHAR(255) NOT NULL,
-    FOREIGN KEY (fk_question_id) REFERENCES questions(id)
-);
-
