bdbddad155cc49c8a6ad7b9e6df13fc5ee58d9f9
Christian <3003html@gmail.com>
2022-08-05 18:24:54 -0500

    score attribute deleted from enrollment struct in models

--- models/models.go
+++ models/models.go
@@ -15,6 +15,7 @@ type Exam struct {
 
 // Model of enrollment
 type Enrollment struct {
+	Id        string `json:"id"`
 	StudentId string `json:"fk_student_id"`
 	ExamId    string `json:"fk_exam_id"`
 }
