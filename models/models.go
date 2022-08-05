d4304d1632823c6934d97a48c76259b33830b657
Christian <3003html@gmail.com>
2022-08-04 20:46:26 -0500

    json struct modified for models that have foreign keys

--- models/models.go
+++ models/models.go
@@ -13,16 +13,15 @@ type Exam struct {
 	Name string `json:"name"`
 }
 
+// Model of enrollment
+type Enrollment struct {
+	StudentId string `json:"fk_student_id"`
+	ExamId    string `json:"fk_exam_id"`
+}
+
 // Model of question
 type Question struct {
 	Id       string `json:"id"`
 	Question string `json:"question"`
-	Answer   string `json:"answer"`
-	ExamId   string `json:"exam_id"`
-}
-
-// Model of enrollment
-type Enrollment struct {
-	StudentId string `json:"student_id"`
-	ExamId    string `json:"exam_id"`
+	ExamId   string `json:"fk_exam_id"`
 }
